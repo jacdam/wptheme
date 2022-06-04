ecf6e549a2022c6cf34266a494cfa8597c5b063e
Jacopo Damiani <jacopo.damiani@gmail.com>
2022-06-04 13:11:10 +0200

    Wordpress and first draft of theme

---
+++ wp-content/themes/mytheme/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Wp Theme</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header_top">
+                <div class="logo">JacopoDamiani</div>
+                <nav>
+                    <ul>
+                        <li>Chi sono</li>
+                        <li>Articoli</li>
+                        <li>Portfolio</li>
+                        <li>Contattami</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="article_title">
+                <h1>How To Build A Group Chat App With Vanilla JS, Twilio And Node.js</h1>   
+            </div>
+        </div>
+    </header>   
+    <main>
+        <article class="container">
+            <div class="article_image">
+                <img src="./img/article_image.png" alt="Main image" />
+            </div>
+            <div class="article_content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium sollicitudin lectus et venenatis. Sed at urna at leo condimentum consectetur. Phasellus nec maximus ante. Cras sit amet hendrerit sem. Morbi fringilla arcu turpis, a posuere quam porta eget. Phasellus tempus metus eget consequat consectetur. Morbi sed sem laoreet, molestie lectus quis, gravida mauris. Nam vestibulum lorem quis pellentesque pharetra. Nam facilisis non dolor id laoreet. Quisque congue ut turpis eget condimentum. Nunc molestie nec leo in dapibus. Vestibulum auctor nibh eget est venenatis, non sagittis nunc viverra.</p> 
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium sollicitudin lectus et venenatis. Sed at urna at leo condimentum consectetur. Phasellus nec maximus ante. Cras sit amet hendrerit sem. Morbi fringilla arcu turpis, a posuere quam porta eget. Phasellus tempus metus eget consequat consectetur. Morbi sed sem laoreet, molestie lectus quis, gravida mauris. Nam vestibulum lorem quis pellentesque pharetra. Nam facilisis non dolor id laoreet. Quisque congue ut turpis eget condimentum. Nunc molestie nec leo in dapibus. Vestibulum auctor nibh eget est venenatis, non sagittis nunc viverra.</p> 
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium sollicitudin lectus et venenatis. Sed at urna at leo condimentum consectetur. Phasellus nec maximus ante. Cras sit amet hendrerit sem. Morbi fringilla arcu turpis, a posuere quam porta eget. Phasellus tempus metus eget consequat consectetur. Morbi sed sem laoreet, molestie lectus quis, gravida mauris. Nam vestibulum lorem quis pellentesque pharetra. Nam facilisis non dolor id laoreet. Quisque congue ut turpis eget condimentum. Nunc molestie nec leo in dapibus. Vestibulum auctor nibh eget est venenatis, non sagittis nunc viverra.</p> 
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium sollicitudin lectus et venenatis. Sed at urna at leo condimentum consectetur. Phasellus nec maximus ante. Cras sit amet hendrerit sem. Morbi fringilla arcu turpis, a posuere quam porta eget. Phasellus tempus metus eget consequat consectetur. Morbi sed sem laoreet, molestie lectus quis, gravida mauris. Nam vestibulum lorem quis pellentesque pharetra. Nam facilisis non dolor id laoreet. Quisque congue ut turpis eget condimentum. Nunc molestie nec leo in dapibus. Vestibulum auctor nibh eget est venenatis, non sagittis nunc viverra.</p> 
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            </div>
+        </article>
+    </main>
+</body>
+</html>
